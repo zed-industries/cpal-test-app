3e1a49053a2455b14b48b8ec376466c686a89463
Mikayla <mikayla@zed.dev>
2024-11-20 11:46:03 -0800

    make more debug output

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,18 @@ use cpal::traits::{DeviceTrait, HostTrait};
 
 fn main() {
     let host = cpal::default_host();
+    println!(
+        "default input: {:?}",
+        host.default_input_device().unwrap().name()
+    );
     for device in host.input_devices().unwrap() {
-        println!("devices found: {:?}", device.name());
+        println!("input devices found: {:?}", device.name());
+    }
+    println!(
+        "default output: {:?}",
+        host.default_output_device().unwrap().name()
+    );
+    for device in host.output_devices().unwrap() {
+        println!("output devices found: {:?}", device.name());
     }
 }
